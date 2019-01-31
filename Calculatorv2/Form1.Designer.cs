ac8b2bed732274c1ed7b2abe635a98674b9a3036
YanickGit <46899594+YanickGit@users.noreply.github.com>
2019-01-31 13:32:39 -0500

    Calc_Blank

--- Calculatorv2/Form1.Designer.cs
+++ Calculatorv2/Form1.Designer.cs
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tb_Main = new System.Windows.Forms.TextBox();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb_Main
+            // 
+            this.tb_Main.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Main.Location = new System.Drawing.Point(12, 12);
+            this.tb_Main.Name = "tb_Main";
+            this.tb_Main.Size = new System.Drawing.Size(260, 26);
+            this.tb_Main.TabIndex = 0;
+            // 
+            // btn_1
+            // 
+            this.btn_1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Lime;
+            this.btn_1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Aqua;
+            this.btn_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_1.Location = new System.Drawing.Point(12, 262);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(36, 28);
+            this.btn_1.TabIndex = 1;
+            this.btn_1.Text = "1";
+            this.btn_1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(284, 305);
+            this.Controls.Add(this.btn_1);
+            this.Controls.Add(this.tb_Main);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tb_Main;
+        private System.Windows.Forms.Button btn_1;
     }
 }
 
